c9cadadb69857a1dc96cc45cf5423efb922347ca
poran-dip <porandip0@gmail.com>
2026-05-31 23:23:27 +0530

    chore: add session provider to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,25 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { SessionProvider } from "next-auth/react";
 import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
-  title: "EZ Doc",
-  description: "Prisma dev testing",
+  title: "FullHP",
+  description:
+    "Your complete healthcare companion — book appointments, connect with doctors, and get emergency support, all in one place.",
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <head />
       <body>
-        <main>{children}</main>
-        <Toaster />
+        <SessionProvider>
+          <main>{children}</main>
+          <Toaster />
+        </SessionProvider>
       </body>
     </html>
   );
