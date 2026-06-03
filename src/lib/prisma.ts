3c5882dec1e9096ee50d265f0835174a000a209d
poran-dip <porandip0@gmail.com>
2026-06-03 22:39:48 +0530

    chore: remove prisma logging

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -17,10 +17,6 @@ export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     adapter,
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "error", "warn"]
-        : ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
